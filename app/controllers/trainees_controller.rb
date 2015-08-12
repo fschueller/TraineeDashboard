e1e6e6fb86ac6c38c894ac508d157333c75ee3a5
farahschueller <Farah@lp750-4superveloce.site>
2015-08-12 16:54:45 +0200

    inlcuded index, new, create action

--- app/controllers/trainees_controller.rb
+++ app/controllers/trainees_controller.rb
@@ -1,11 +1,28 @@
 class TraineesController < ApplicationController
   def index
+    @trainees = Trainee.all
   end
 
   def new
+    @trainee = Trainee.new
   end
 
   def create
+    @trainee = Trainee.new(trainee_params)
   end
-  
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  protected
+
+  def trainee_params
+    params.require(:trainee).permit(:first_name, :last_name, :birthday, :year, :email, :phone, :suse_login, :github, :trello, :description)
+  end
 end
