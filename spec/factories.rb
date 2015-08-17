71797229cef2fb1190ff8cd727f5356f19b73a9b
farahschueller <Farah@lp750-4superveloce.site>
2015-08-17 15:30:16 +0200

    edited factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :trainee, class: Trainee do
     sequence(:first_name) { Faker::Name.first_name }
     sequence(:last_name) { Faker::Name.last_name }
-    sequence(:birthday) { Date.today.to_date }
-    sequence(:year) { Faker::Number.digit }
+    sequence(:birthday) { Date.new.to_formatted_s(:iso8601) }
+    sequence(:year) { 1 }
     sequence(:email) { Faker::Internet.email }
     sequence(:phone) { Faker::PhoneNumber.phone_number }
     sequence(:suse_login) { Faker::Internet.user_name(%w(. _ -)) }
