662196b7e9211780c81073ad4b66f9a8ec76340d
farahschueller <Farah@lp750-4superveloce.site>
2015-08-19 16:39:18 +0200

    added email validation

--- app/models/trainee.rb
+++ app/models/trainee.rb
@@ -1,4 +1,5 @@
 class Trainee < ActiveRecord::Base
   validates :first_name, :last_name, :birthday, :year, :email, :phone, :suse_login, :github, :trello, :description, presence: :true
-
+  validates :email, :email => true
+  validates :year, :inclusion => 1..3
 end
